7c57e36b35cef1cc37c5ff5bbb888d10d8d9ce4e
kadijab <kadija_bell@yahoo.com>
2022-04-30 18:57:51 -0400

    Draft 1 - trying to add transitions

---
+++ scripts.js
@@ -0,0 +1,18 @@
+console.log("Sanity Check")
+
+
+
+let wordList = ["Cat", "Fence", "Paint"];
+function getRandomInt(max) {
+return Math.floor(Math.random() * max);
+}
+
+let displayWord = ["", "E", "", "", "E"];
+let currentPoints = 0;
+let secretWord = "Fence";
+let maxPoints = 6;
+let guessedLetters = ["J"];
+
+
+document.body.style.backgroundImage= "/Users/zakiyabell/sei/projects/Deep-State-/images/starting_background.jpeg"
+
