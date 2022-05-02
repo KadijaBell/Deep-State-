534bb3b0b670f9200d355fde21fdb6f7df697ced
kadijab <kadija_bell@yahoo.com>
2022-05-01 21:11:40 -0400

    game layout

--- scripts.js
+++ scripts.js
@@ -2,17 +2,27 @@ console.log("Sanity Check")
 
 
 
-let wordList = ["Cat", "Fence", "Paint"];
-function getRandomInt(max) {
-return Math.floor(Math.random() * max);
+// let wordList = ["Cat", "Fence", "Paint"];
+// function getRandomInt(max) {
+// return Math.floor(Math.random() * max);
+// }
+
+// let displayWord = ["", "E", "", "", "E"];
+// let currentPoints = 0;
+// let secretWord = "Fence";
+// let maxPoints = 6;
+// let guessedLetters = ["J"];
+
+
+//Game Play
+
+//Function for questions
+function deepState (questions){
+    this.score = 0;
+    this.questions = questions;
 }
 
-let displayWord = ["", "E", "", "", "E"];
-let currentPoints = 0;
-let secretWord = "Fence";
-let maxPoints = 6;
-let guessedLetters = ["J"];
 
-
-document.body.style.backgroundImage= "/Users/zakiyabell/sei/projects/Deep-State-/images/starting_background.jpeg"
-
+//Questions
+let gameQuestions =[]
+//How to win
